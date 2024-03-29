47996010512482ab35f0f060af2e2c4b4ca598f2
Silas Abayomi <abayomisilas50@gmail.com>
2024-03-29 23:38:10 +0100

    update

--- src/components/pages/details/Details.jsx
+++ src/components/pages/details/Details.jsx
@@ -10,17 +10,17 @@ const Details = () => {
   return (
     <div className='overflow-x-hidden'>
       <Navbar />
-      <div className="mt-[10rem]">
-        <img src={Image1} alt="" />
+      <div className="mt-[8rem]">
+        <img src={Image1} alt="" className='md:w-full md:h-full h-[200px]'/>
       </div>
-      <div className='flex mx-[8vw] mt-[5vh]'>
+      <div className='flex md:flex-row flex-col md:gap[0px] gap-[10px] mx-[8vw] mt-[5vh]'>
         <div className="flex flex-col gap-[15px]">
           <div className='flex gap-8 items-center'>
             <h1 className='text-[35px] font-[500]'>3 Bedroom Apartment</h1>
-            <p className='flex items-center text-[15px] text-primary-AffilestateBlue60 font-[400] bg-primary-AffilestateBlue10 py-[5px] px-[10px]'>For Sale</p>
+            <p className='md:text-[15px] text-[10px] text-primary-AffilestateBlue60 font-[400] bg-primary-AffilestateBlue10 md:py-[5px] py-[15px] px-[10px] md:w-[8%] w-[25%]'>For Sale</p>
           </div>
           <div>
-            <p className='text-[30px] font-[300] w-[55%]'>
+            <p className='text-[30px] font-[300] md:w-[55%]'>
               Description: A 3 bedroom fully furnished apartment for sale.
               Located in the heart of Lagos Island , the hub of business
             </p>
@@ -35,14 +35,14 @@ const Details = () => {
           <div className='flex md:flex-row flex-col gap-10'>
             <p className='border'>I found this useful</p>
             <p className='border'>This is within my budget</p>
-            <p className='border'>This is beyond my budge</p>
+            <p className='border'>This is beyond my budget</p>
             <p className='border'>I love this location</p>
           </div>
         </div>
 
         <div className='flex flex-col gap-[10px] whitespace-nowrap'>
           <p className='text-[35px] font-[600]'>N 300,000.00</p>
-          <p className='flex gap-[12px] items-center bg-primary-AffilestateBlue60 text-white w-[90%] px-[10px] py-[10px] ml-[18px] font-[300]'>
+          <p className='flex gap-[12px] items-center bg-primary-AffilestateBlue60 text-white md:w-[90%] w-[60%] px-[10px] py-[10px] md:ml-[18px] font-[300]'>
             <FiPhone  size={20} color='white'/>
             CONTACT AGENT
           </p>
